0860bc9fc4c15d08fb91a657dca9b80cb909eb28
Milan Rawat <milanrawat086@gmail.com>
2021-02-10 16:52:46 +0530

    Created sellerSchema

---
+++ server/models/sellerModel.js
@@ -0,0 +1,114 @@
+const mongoose = require("mongoose");
+
+const sellerSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please tell us your name!"],
+    },
+    lastName: String,
+    email: {
+      type: String,
+      required: [true, "Please provide your email!"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email!"],
+    },
+    photo: {
+      type: String,
+      default: "default.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["user", "seller", "admin"],
+      default: "seller",
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords are not the same!",
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    sellingProducts: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+      },
+    ],
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+sellerSchema.pre("save", async function (next) {
+  // Only run  this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 16);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+sellerSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+sellerSchema.pre(/^find/, function (next) {
+  // this points to current query
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+sellerSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+sellerSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimeStamp;
+  }
+
+  //   False means NOT changed
+  return false;
+};
+
+const Seller = mongoose.model("Seller", sellerSchema);
+
+module.exports = Seller;
