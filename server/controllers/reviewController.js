53f3e52c56b697ca5662d1aab06bcfc58f1a1a7d
Milan-rawat <milanrawat086@gmail.com>
2021-02-17 08:55:51 +0530

    review controls created

---
+++ server/controllers/reviewController.js
@@ -0,0 +1,57 @@
+const Review = require("../models/reviewModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find({});
+
+  res.status(200).json({
+    status: "success",
+    results: reviews.length,
+    data: reviews,
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: newReview,
+  });
+});
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const updatedReview = await Review.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(204).json({
+    status: "success",
+    data: updatedReview,
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: newReview,
+  });
+});
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  await Review.findOneAndUpdate(req.user.id, {
+    active: false,
+  });
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
