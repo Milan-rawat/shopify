82fe3b8b41a4d11f4cd802d459cb1256698b272b
Milan-rawat <milanrawat086@gmail.com>
2021-03-07 11:53:53 +0530

    create, update & delete review protected

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -4,17 +4,27 @@ const authController = require("./../controllers/authController");
 
 const router = express.Router({ mergeParams: true });
 
-router.use(authController.protect);
-
 router
   .route("/")
   .get(reviewController.getAllReviews)
-  .post(authController.allowedTo("user"), reviewController.createReview);
+  .post(
+    authController.protect,
+    authController.allowedTo("user"),
+    reviewController.createReview
+  );
 
 router
   .route("/:id")
   .get(reviewController.getReview)
-  .patch(authController.allowedTo("user"), reviewController.updateReview)
-  .delete(authController.allowedTo("user"), reviewController.deleteReview);
+  .patch(
+    authController.protect,
+    authController.allowedTo("user"),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.protect,
+    authController.allowedTo("user"),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
