3088930b62967ca235d3587bb263c55f4d58cc3a
Milan-rawat <milanrawat086@gmail.com>
2021-04-28 16:12:49 +0530

    Category

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
 import Main from "../components/Main/Main";
+import Footer from "../components/Footer/Footer";
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
       <div>
         <Header />
         <Main />
+        <Footer />
       </div>
     );
   }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,6 +12,46 @@ class Main extends Component {
     return (
       <>
         <div className="homeMain">
+          <div className="categoryBox">
+            {/* <div className="categories"> */}
+            <div className="category">
+              <h1>1</h1>
+            </div>
+            <div className="category">
+              <h1>2</h1>
+            </div>
+            <div className="category">
+              <h1>3</h1>
+            </div>
+            <div className="category">
+              <h1>4</h1>
+            </div>
+            <div className="category">
+              <h1>5</h1>
+            </div>
+            <div className="category">
+              <h1>6</h1>
+            </div>
+            <div className="category">
+              <h1>7</h1>
+            </div>
+            <div className="category">
+              <h1>8</h1>
+            </div>
+            <div className="category">
+              <h1>9</h1>
+            </div>
+            <div className="category">
+              <h1>10</h1>
+            </div>
+            <div className="category">
+              <h1>11</h1>
+            </div>
+            <div className="category">
+              <h1>12</h1>
+            </div>
+            {/* </div> */}
+          </div>
           <div className="bannerSlider">
             <Slider {...bannerSettings}>
               <div className="banner">
