ffcffb00624d23f418ffc09374f7a78394706412
Milan-rawat <milanrawat086@gmail.com>
2021-04-28 16:02:35 +0530

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <>
+        <div className="Footer">2021 Shopify &copy;;</div>
+      </>
+    );
+  }
+}
+
+export default Footer;
